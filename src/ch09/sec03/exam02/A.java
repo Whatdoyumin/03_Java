abde12d57ecc51d6ab89392fe9c2448609dd7394
Whatdoyumin <lym4963@naver.com>
2025-04-22 14:33:06 +0900

    [2주차] 중첩 객체 기본 구현

---
+++ src/ch09/sec03/exam02/A.java
@@ -0,0 +1,24 @@
+package ch09.sec03.exam02;
+
+public class A {
+    // 정적 멤버 클래스
+    static class B {
+        static int field2 = 2;
+        int field1 = 1;
+
+        B() {
+
+            System.out.println("B-생성자 실행");
+        }
+
+        static void method2() {
+
+            System.out.println("B-method2 실행");
+        }
+
+        void method1() {
+
+            System.out.println("B-method1 실행");
+        }
+    }
+}
