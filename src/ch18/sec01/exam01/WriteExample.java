6f9576564f3d9470bece53d5ed9def1768f8d358
Whatdoyumin <lym4963@naver.com>
2025-05-10 23:01:30 +0900

    [4주차] IO 실습 구현

---
+++ src/ch18/sec01/exam01/WriteExample.java
@@ -0,0 +1,26 @@
+package ch18.sec01.exam01;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteExample {
+    public static void main(String[] args) {
+        try {
+            OutputStream os = new FileOutputStream("/Users/whatdoyumin/test1.db");
+
+            byte a = 65;
+            byte b = 66;
+            byte c = 67;
+
+            os.write(a);
+            os.write(b);
+            os.write(c);
+
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
